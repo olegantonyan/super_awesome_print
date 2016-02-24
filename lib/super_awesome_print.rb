b01fe7072c1d17f10f4274e1a9033e3ce7a3d16a
Mike Kunkel <mike@mkunkel.com>
2016-02-24 09:38:23 -0600

    Update to follow the Ruby Style Guide
    https://github.com/bbatsov/ruby-style-guide
    
    Ignored conventions:
     * Line length
     * Top level class/module documentation

--- lib/super_awesome_print.rb
+++ lib/super_awesome_print.rb
@@ -1,13 +1,13 @@
-require "super_awesome_print/version"
-require "awesome_print"
+require 'super_awesome_print/version'
+require 'awesome_print'
 
-def sap msg
-  ap "*** #{Time.now} ***", color: {string: :green}
+def sap(msg)
+  ap "*** #{Time.now} ***", color: { string: :green }
   ap msg.class if msg.respond_to?(:class)
   src = caller.first.gsub(Rails.root.to_s + '/', '')
-  ap src, color: {string: :purpleish}
+  ap src, color: { string: :purpleish }
   ap msg
-  ap '*** END ***', color: {string: :green}
+  ap '*** END ***', color: { string: :green }
 end
 
 module SuperAwesomePrint
